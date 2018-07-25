ecc71699122b9d0d7ba167539cb19da8c5600a95
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-25 13:47:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,8 @@ def key_for_min_value(name_hash)
     lowest_key = nil
     lowest_value = nil
     name_hash.each do |name, value|
+      if lowest_value == nil || value < lowest_value
+        lowest_value = value
+        lowest_key = name
 end
 end
